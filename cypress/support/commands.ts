2395bb45773e98f42e2186439fdc48b5e876ebc4
Julius Stenbæk Christoffersen <julius@adnudging.com>
2022-12-21 18:13:02 +0100

    removed mount override

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,15 +1,4 @@
 /* eslint-disable @typescript-eslint/no-namespace */
-import { mount } from "cypress/vue";
-import { createPinia } from "pinia";
-import { createRouter, createMemoryHistory } from "vue-router";
-import { useUserStore } from "../../src/stores/user";
-import router from "../../src/router/index";
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import type { App } from "vue";
-
-const routes = router.getRoutes();
-
 /// <reference types="cypress" />
 
 declare global {
@@ -28,31 +17,6 @@ Cypress.Commands.add("login", () => {
   cy.get('[data-cy="login-button"]').click();
 
   cy.get('[data-cy="logout"]').should("exist");
-});
-
-Cypress.Commands.add("mount", (component, options = {}) => {
-  const router = createRouter({
-    routes,
-    history: createMemoryHistory(),
-  });
-
-  const installRouterPlugin = (app: App) => {
-    app.use(router);
-  };
-
-  options = {
-    global: {
-      plugins: [installRouterPlugin],
-    },
-    ...options,
-  };
-
-  options.path && cy.wrap(router.push(options.path));
-
-  const pinia = createPinia();
-  options.store || useUserStore(pinia);
-
-  return mount(component, options);
 });
 
 export {};
